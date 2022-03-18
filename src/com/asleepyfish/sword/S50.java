3d2d71a198720f58dfc130a667f7d58a03b8bc39
zhoujh42045 <zhoujh42045@hundsun.com>
2022-03-18 18:06:19 +0800

    answer： S03.数组中重复的数字 S53I.在排序数组中查找数字 I S53II.0～n-1中缺失的数字

---
+++ src/com/asleepyfish/sword/S50.java
@@ -0,0 +1,21 @@
+package com.asleepyfish.sword;
+
+/**
+ * @Author: zhoujh42045
+ * @Date: 2022/3/17 20:00
+ * @Description: 第一个只出现一次的字符
+ * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+ * 示例 1:
+ * 输入：s = "abaccdeff"
+ * 输出：'b'
+ * 示例 2:
+ * 输入：s = ""
+ * 输出：' '
+ * 限制：
+ * 0 <= s 的长度 <= 50000
+ */
+public class S50 {
+    public char firstUniqChar(String s) {
+        return ' ';
+    }
+}
