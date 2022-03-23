6ec615651d71642c1ab20bd12845b01ab59dfa23
asleepyfish <asleepyfish@foxmail.com>
2022-03-23 21:37:35 +0800

    Question： S10-I.斐波那契数列 S10-II.青蛙跳台阶问题 S63.股票的最大利润

--- src/com/asleepyfish/sword/S26.java
+++ src/com/asleepyfish/sword/S26.java
@@ -10,13 +10,13 @@ import com.asleepyfish.TreeNode;
  * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
  * 例如:
  * 给定的树 A:
- *    3
- *   / \
- *  4  5
+ * 3
+ * / \
+ * 4  5
  * / \
  * 1  2
  * 给定的树 B：
- *  4
+ * 4
  * /
  * 1
  * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
@@ -31,6 +31,16 @@ import com.asleepyfish.TreeNode;
  */
 public class S26 {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        return false;
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) {
+            return true;
+        }
+        if (A == null || A.val != B.val) {
+            return false;
+        }
+        return recur(A.left, B.left) && recur(A.right, B.right);
     }
 }
--- src/com/asleepyfish/sword/S27.java
+++ src/com/asleepyfish/sword/S27.java
@@ -2,13 +2,15 @@ package com.asleepyfish.sword;
 
 import com.asleepyfish.TreeNode;
 
+import java.util.Stack;
+
 /**
  * @Author: asleepyfish
  * @Date: 2022-03-22 21:37
  * @Description: 二叉树的镜像
  * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
  * 例如输入：
- *   4
+ * 4
  * /  \
  * 2   7
  * / \  / \
@@ -27,6 +29,36 @@ import com.asleepyfish.TreeNode;
  */
 public class S27 {
     public TreeNode mirrorTree(TreeNode root) {
-        return null;
+        if (root == null) {
+            return null;
+        }
+        // 在递归右子节点 “root.left = mirrorTree(root.right);root.left=mirrorTree(root.right);”
+        // 执行完毕后， root.leftroot.left 的值已经发生改变，此时递归左子节点 mirrorTree(root.left)mirrorTree(root.left) 则会出问题。
+        TreeNode tmp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(tmp);
+        return root;
+    }
+
+    public TreeNode mirrorTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack() {{
+            add(root);
+        }};
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+        return root;
     }
 }
