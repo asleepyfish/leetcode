ca3bcde1c24d5d0fc8598bcb036498fdc5de3bdd
zhoujh42045 <zhoujh42045@hundsun.com>
2022-03-25 17:53:49 +0800

    answer： S04.二维数组中的查找 S11.旋转数组的最小数字 S50.第一个只出现一次的字符

--- src/com/asleepyfish/sword/S42.java
+++ src/com/asleepyfish/sword/S42.java
@@ -16,6 +16,13 @@ package com.asleepyfish.sword;
  */
 public class S42 {
     public int maxSubArray(int[] nums) {
-        return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 }
