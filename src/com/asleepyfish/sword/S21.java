65b5c833bdfd3dfa5546eafe73a72f468bd1d6ec
asleepyfish <asleepyfish@foxmail.com>
2022-04-01 20:42:33 +0800

    Question

--- src/com/asleepyfish/sword/S21.java
+++ src/com/asleepyfish/sword/S21.java
@@ -30,4 +30,20 @@ public class S21 {
         }
         return res;
     }
+
+    public int[] exchange2(int[] nums) {
+        int i = 0, j = nums.length - 1, tmp;
+        while (i < j) {
+            while (i < j && (nums[i] & 1) == 1) {
+                i++;
+            }
+            while (i < j && (nums[j] & 1) == 0) {
+                j--;
+            }
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+        return nums;
+    }
 }
--- src/com/asleepyfish/sword/S58I.java
+++ src/com/asleepyfish/sword/S58I.java
@@ -34,4 +34,26 @@ public class S58I {
         sb.append(split[0]);
         return sb.toString();
     }
+
+    public String reverseWords2(String s) {
+        // 删除首尾空格
+        s = s.trim();
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') {
+                // 搜索首个空格
+                i--;
+            }
+            // 添加单词
+            res.append(s, i + 1, j + 1).append(" ");
+            while(i >= 0 && s.charAt(i) == ' ') {
+                i--; // 跳过单词间空格
+            }
+            // j 指向下个单词的尾字符
+            j = i;
+        }
+        // 转化为字符串并返回
+        return res.toString().trim();
+    }
 }
