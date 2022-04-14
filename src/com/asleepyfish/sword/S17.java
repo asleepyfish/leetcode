26984a8d1b6921d6e8ad9a988e14d2d176fde040
asleepyfish <asleepyfish@foxmail.com>
2022-04-14 23:06:18 +0800

    Question

--- src/com/asleepyfish/sword/S17.java
+++ src/com/asleepyfish/sword/S17.java
@@ -1,5 +1,8 @@
 package com.asleepyfish.sword;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Author: asleepyfish
  * @Date: 2022-04-13 23:17
@@ -15,5 +18,64 @@ package com.asleepyfish.sword;
 public class S17 {
     public int[] printNumbers(int n) {
         return null;
+    }
+
+    int[] res;
+    int nine = 0, count = 0, start, n;
+    char[] num, loop = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    public int[] printNumbers2(int n) {
+        this.n = n;
+        res = new int[(int) Math.pow(10, n) - 1];
+        num = new char[n];
+        start = n - 1;
+        dfs(0);
+        return res;
+    }
+
+    void dfs(int x) {
+        if (x == n) {
+            String s = String.valueOf(num).substring(start);
+            if (!"0".equals(s)) {
+                res[count++] = Integer.parseInt(s);
+            }
+            if (n - start == nine) {
+                start--;
+            }
+            return;
+        }
+        for (char i : loop) {
+            if (i == '9') {
+                nine++;
+            }
+            num[x] = i;
+            dfs(x + 1);
+        }
+        nine--;
+    }
+
+    public int[] printNumbers3(int n) {
+        int[] ans = new int[(int) (Math.pow(10, n)) - 1];
+        Queue<String> q = new LinkedList<>();
+        //将1~9加入队列
+        for (int i = 1; i <= 9; i++) {
+            q.offer(String.valueOf(i));
+        }
+        int index = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                //将队列中的元素依次取出并加入答案
+                String str = q.poll();
+                ans[index++] = Integer.parseInt(str);
+                //若当前元素长度不够n，则在末尾补充0~9，继续加入队列
+                if (str.length() < n) {
+                    for (int j = 0; j <= 9; j++) {
+                        q.offer(str + j);
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
--- src/com/asleepyfish/sword/S19.java
+++ src/com/asleepyfish/sword/S19.java
@@ -39,6 +39,80 @@ package com.asleepyfish.sword;
  */
 public class S19 {
     public boolean isMatch(String s, String p) {
-        return false;
+        int n = s.length();
+        int m = p.length();
+        boolean[][] res = new boolean[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                //分成空正则和非空正则两种
+                if (j == 0) {
+                    res[i][j] = i == 0;
+                } else {
+                    //非空正则分为两种情况 * 和 非*
+                    if (p.charAt(j - 1) != '*') {
+                        if (i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.')) {
+                            res[i][j] = res[i - 1][j - 1];
+                        }
+                    } else {
+                        //碰到 * 了，分为看和不看两种情况
+                        //不看
+                        if (j >= 2) {
+                            res[i][j] |= res[i][j - 2];
+                        }
+                        //看
+                        if (i >= 1 && j >= 2 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')) {
+                            res[i][j] |= res[i - 1][j];
+                        }
+                    }
+                }
+            }
+        }
+        return res[n][m];
+    }
+
+    public boolean isMatch2(String s, String p) {
+        // 如果字符串长度为0，需要检测下正则串
+        if (s.length() == 0) {
+            // 如果正则串长度为奇数，必定不匹配，比如 "."、"ab*",必须是 a*b*这种形式，*在奇数位上
+            if (p.length() % 2 != 0) {
+                return false;
+            }
+            int i = 1;
+            while (i < p.length()) {
+                if (p.charAt(i) != '*') {
+                    return false;
+                }
+                i += 2;
+            }
+            return true;
+        }
+        // 如果字符串长度不为0，但是正则串没了，return false
+        if (p.length() == 0) {
+            return false;
+        }
+        // c1 和 c2 分别是两个串的当前位，c3是正则串当前位的后一位，如果存在的话，就更新一下
+        char c1 = s.charAt(0), c2 = p.charAt(0), c3 = 'a';
+        if (p.length() > 1) {
+            c3 = p.charAt(1);
+        }
+        // 和dp一样，后一位分为是 '*' 和不是 '*' 两种情况
+        if (c3 != '*') {
+            // 如果该位字符一样，或是正则串该位是 '.',也就是能匹配任意字符，就可以往后走
+            if (c1 == c2 || c2 == '.') {
+                return isMatch2(s.substring(1), p.substring(1));
+            } else {
+                // 否则不匹配
+                return false;
+            }
+        } else {
+            // 如果该位字符一样，或是正则串该位是 '.'，和dp一样，有看和不看两种情况
+            if (c1 == c2 || c2 == '.') {
+                return isMatch2(s.substring(1), p) || isMatch2(s, p.substring(2));
+            } else {
+                // 不一样，那么正则串这两位就废了，直接往后走
+                return isMatch2(s, p.substring(2));
+            }
+        }
     }
 }
