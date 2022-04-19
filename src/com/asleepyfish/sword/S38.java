5ab83ac4edea6dff552024338064f7c0a7e54338
asleepyfish <asleepyfish@foxmail.com>
2022-04-19 23:45:24 +0800

    Question

--- src/com/asleepyfish/sword/S38.java
+++ src/com/asleepyfish/sword/S38.java
@@ -1,5 +1,10 @@
 package com.asleepyfish.sword;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @Author: asleepyfish
  * @Date: 2022-04-17 21:48
@@ -13,7 +18,40 @@ package com.asleepyfish.sword;
  * 1 <= s 的长度 <= 8
  */
 public class S38 {
+    List<String> res = new LinkedList<>();
+    char[] c;
+
     public String[] permutation(String s) {
-        return null;
+        c = s.toCharArray();
+        dfs(0);
+        return res.toArray(new String[0]);
+    }
+
+    void dfs(int x) {
+        if (x == c.length - 1) {
+            // 添加排列方案
+            res.add(String.valueOf(c));
+            return;
+        }
+        Set<Character> set = new HashSet<>();
+        for (int i = x; i < c.length; i++) {
+            if (set.contains(c[i])) {
+                // 重复，因此剪枝
+                continue;
+            }
+            set.add(c[i]);
+            // 交换，将 c[i] 固定在第 x 位
+            swap(i, x);
+            // 开启固定第 x + 1 位字符
+            dfs(x + 1);
+            // 恢复交换
+            swap(i, x);
+        }
+    }
+
+    void swap(int a, int b) {
+        char tmp = c[a];
+        c[a] = c[b];
+        c[b] = tmp;
     }
 }
