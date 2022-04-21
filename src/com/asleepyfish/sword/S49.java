38df7e35060fcb430b811f66e4f3e86a7fb9e3d2
asleepyfish <asleepyfish@foxmail.com>
2022-04-21 20:58:40 +0800

    Question

---
+++ src/com/asleepyfish/sword/S49.java
@@ -0,0 +1,20 @@
+package com.asleepyfish.sword;
+
+/**
+ * @Author: asleepyfish
+ * @Date: 2022-04-21 20:52
+ * @Description: 丑数
+ * 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+ * 示例:
+ * 输入: n = 10
+ * 输出: 12
+ * 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+ * 说明:  
+ * 1是丑数。
+ * n不超过1690。
+ */
+public class S49 {
+    public int nthUglyNumber(int n) {
+        return -1;
+    }
+}
---
+++ src/com/asleepyfish/sword/S56II.java
@@ -0,0 +1,22 @@
+package com.asleepyfish.sword;
+
+/**
+ * @Author: asleepyfish
+ * @Date: 2022-04-21 20:55
+ * @Description: 数组中数字出现的次数 II
+ * 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+ * 示例 1：
+ * 输入：nums = [3,4,3,3]
+ * 输出：4
+ * 示例 2：
+ * 输入：nums = [9,1,7,9,7,9,7]
+ * 输出：1
+ * 限制：
+ * 1 <= nums.length <= 10000
+ * 1 <= nums[i] < 2^31
+ */
+public class S56II {
+    public int singleNumber(int[] nums) {
+        return -1;
+    }
+}
