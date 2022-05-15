2a0bb1bc2e9bb65976b29ae7251eccf50e9ce188
asleepyfish <asleepyfish@foxmail.com>
2022-05-15 22:57:21 +0800

    Question: S62 S66

--- src/com/asleepyfish/sword/S60.java
+++ src/com/asleepyfish/sword/S60.java
@@ -35,4 +35,28 @@ public class S60 {
         }
         return dp;
     }
+
+    public double[] dicesProbability2(int n) {
+        int[][] dp = new int[n + 1][6 * n + 1];
+        for (int i = 1; i <= 6; i++) {
+            dp[1][i] = 1;
+        }
+        // i表示投掷多少颗骰子
+        for (int i = 2; i <= n; i++) {
+            // i = 2时候，j从2开始到12结束，共11中可能出现的数字
+            for (int j = i; j <= 6 * i; j++) {
+                // 新加入的骰子的可能范围是1-6，但是k需要小于j保证j - k > 0。这样可以按照前i - 1步的数字得出结果
+                for (int k = 1; k <= 6 && k < j; k++) {
+                    dp[i][j] += dp[i - 1][j - k];
+                }
+            }
+        }
+        double[] ans = new double[6 * n - n + 1];
+        // i从n 到6n就是骰子可能取到的所有数字
+        for (int i = n; i <= 6 * n; i++) {
+            // 从0开始，dp[n][6n]为n个骰子，投处数字为6n的概率
+            ans[i - n] = ((double) dp[n][i]) / (Math.pow(6, n));
+        }
+        return ans;
+    }
 }
