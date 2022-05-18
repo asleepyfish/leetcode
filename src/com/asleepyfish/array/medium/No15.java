02c8fe41f793ac793f8f397288d5e909a361da0f
asleepyfish <asleepyfish@foxmail.com>
2022-05-18 22:08:32 +0800

    Question: 56 75 191 706 1281

--- src/com/asleepyfish/array/medium/No15.java
+++ src/com/asleepyfish/array/medium/No15.java
@@ -58,4 +58,52 @@ public class No15 {
         }
         return false;
     }
+
+    /**
+     * 总时间复杂度：O(n^2)
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length <= 2) {
+            return ans;
+        }
+        // O(nlogn)
+        Arrays.sort(nums);
+
+        // O(n^2)
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 第一个数大于 0，后面的数都比它大，肯定不成立了
+            if (nums[i] > 0) {
+                break;
+            }
+            // 去掉重复情况
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = 0, left = 1, right = 6, [-2, -1, 3] 的答案加入后，需要排除重复的 -1 和 3
+                    left++;
+                    // 首先无论如何先要进行加减操作
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
 }
